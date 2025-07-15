2944fed249bdca115f4aaf813a18b49cd5c68c54
Sergio Botelho <sergiorbotelho@gmail.com>
2025-07-14 22:39:40 -0300

    Produto e associacoes muitos para muitos

--- src/main/java/br/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,6 @@
 package br.com.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
